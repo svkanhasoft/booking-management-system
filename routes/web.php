b3f44b1aee0f91d25596a58eb5f9af64172d9fab
shailesh-kanhasoft <shaileshv.kanhasoft@gmail.com>
2021-08-13 18:50:11 +0530

    remove extra files

--- routes/web.php
+++ routes/web.php
@@ -16,26 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::resource('organization/organization', 'Organization\OrganizationController');
-Route::resource('organization/designation', 'Organization\DesignationController');
-Route::resource('organization/role', 'Organization\RoleController');
-Route::resource('organization/organization_user_detail', 'Organization\organization_user_detailController');
-Route::resource('organization/organization-user-detail', 'Organization\OrganizationUserDetailController');
-Route::resource('organization/specialities', 'Organization\SpecialitiesController');
-Route::resource('organization/trusts', 'Organization\TrustsController');
-Route::resource('organization/ward', 'Organization\WardController');
-Route::resource('organization/traning', 'Organization\TraningController');
-Route::resource('signees/signees-detail', 'Signees\SigneesDetailController');
-Route::resource('signee/signee_organization', 'Signee\signee_organizationController');
-Route::resource('signee/signee-organization', 'Signee\SigneeOrganizationController');
-Route::resource('organization/organization-shift', 'Organization\OrganizationShiftController');
-Route::resource('signees/signee-specialitie', 'Signees\SigneeSpecialitieController');
-Route::resource('organization/booking', 'Organization\BookingController');
-Route::resource('organization/booking-speciality', 'Organization\BookingSpecialityController');
-// Route::resource('signees/availability', 'Signees\AvailabilityController');
-Route::resource('organization/candidate-referred-from', 'Organization\CandidateReferredFromController');
