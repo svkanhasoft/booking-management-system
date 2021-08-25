3858a2c8577596e609c4ce13f22a77ebaa77d3c3
maulikkanhasoft <maulik.kanhasoft@gmail.com>
2021-08-25 12:28:19 +0530

    change in ward name field

--- app/Models/Ward.php
+++ app/Models/Ward.php
@@ -28,7 +28,7 @@ class Ward extends Model
      *
      * @var array
      */
-    protected $fillable = ['trust_id', 'ward_na me', 'ward_type', 'ward_number', 'hospital_id'];
+    protected $fillable = ['trust_id', 'ward_name', 'ward_type', 'ward_number', 'hospital_id'];
     // protected $hidden = ['pseudo', 'deleted_at', 'updated_at', 'created_at'];
 
     function addWard($postData, $trustId, $hospital_id, $isDelete = false)
