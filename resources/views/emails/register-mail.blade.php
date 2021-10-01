cd1afd3661891a051c4fc0e9e2167c45d3368834
shailesh-kanhasoft <shaileshv.kanhasoft@gmail.com>
2021-10-01 18:24:30 +0530

    change meial blade file

--- resources/views/emails/register-mail.blade.php
+++ resources/views/emails/register-mail.blade.php
@@ -57,6 +57,7 @@
                                                         <br>
                                                         @else
                                                         <a href="http://adminbooking.kanhasoftdev.com/login">Click here to login.</a>
+                                                        @endif
                                                     </span>
                                                 </p>                                                
                                                 <p class="MsoNormal" style="line-height:145%"><span style="font-size:12pt;line-height:145%;font-family:&quot;Arial&quot;,&quot;sans-serif&quot;;mso-fareast-font-family:&quot;Times New Roman&quot;;color:#37424A">Thank
