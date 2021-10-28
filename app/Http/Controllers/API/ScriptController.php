d504b933c31f2c631e901ffd522e66a58ed7787b
shailesh-kanhasoft <shaileshv.kanhasoft@gmail.com>
2021-10-28 11:21:07 +0530

    change on cron  job

--- app/Http/Controllers/API/ScriptController.php
+++ app/Http/Controllers/API/ScriptController.php
@@ -1,16 +1,22 @@
 <?php
+namespace App\Http\Controllers\API;
 
-namespace App\Http\Controllers\API\Signees;
-
-use App\Http\Controllers\Controller;
-use App\Http\Requests;
-use App\Models\SigneesDetail;
 use Illuminate\Http\Request;
-use App\Models\User;
-use App\Models\Availability;
-use Hash;
-use Validator;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests;
+use App\Models\Hospital;
+use Hash;
+use App\Models\User;
+use Config;
+use DateTime;
+use finfo;
+use Illuminate\Support\Facades\Date;
+use Session;
+use Illuminate\Support\Carbon;
+use PDF;
+use App;
+use App\Models\Booking;
 
 class ScriptController extends Controller
 {
@@ -20,5 +26,29 @@ class ScriptController extends Controller
 
     public function addsignee(Request $request){
         echo "Fsdsd";
+       
     }
+
+    function statusCron()
+    {
+        \Log::info(" Run Status Inactive cronjob ");
+        $userObj = User::select('id', 'email', 'first_name', 'last_login_date')->where('status', 'Active')->where('role', 'SIGNEE')->get()->toArray();
+        foreach ($userObj as $key => $value) {
+            if ($value['last_login_date'] != '' && $value['last_login_date'] != null) {
+                $date1 = new DateTime(date('Y-m-d H:i:s'));
+                $date2 = new DateTime($value['last_login_date']);
+                $interval = $date1->diff($date2);
+                if ($interval->y > 0) {
+                    $userUpdateObj = User::find($value['id']);
+                    $userUpdateObj->status = 'Dormant';
+                    $userUpdateObj->save();
+                } else if ($interval->m > 5) {
+                    $userUpdateObj = User::find($value['id']);
+                    $userUpdateObj->status = 'Inactive';
+                    $userUpdateObj->save();
+                }
+            }
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/sss', [DeleteFilesCron::class, 'handle']);
+Route::get('/status-cron', [App\Http\Controllers\API\ScriptController::class, 'statusCron']);
