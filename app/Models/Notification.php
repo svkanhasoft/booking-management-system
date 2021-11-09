3b5df532215ddb732beadca93b4a6ed6114cb9fa
maulikkanhasoft <maulik.kanhasoft@gmail.com>
2021-11-09 16:26:05 +0530

    change

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -36,8 +36,6 @@ class Notification extends Model
 
     public function addNotification($postData)
     {
-        // print_r($postData);exit();
-        //return true;
         //print_r($postData);exit;
         if($postData['status'] != 'COMPLIANT' && $postData['status'] != 'CREATED' && $postData['status'] != 'CANCEL' && $postData['status'] != 'CONFIRMED' )
         {
@@ -53,9 +51,8 @@ class Notification extends Model
             //print_r($bookingDetails);exit();
             $date = date("d-m-Y", strtotime($bookingDetails['date']));
             $time = date("h:i A", strtotime($bookingDetails['start_time'])).' '.'To'.' '.date("h:i A", strtotime($bookingDetails['end_time']));
-            // dd($postData['status']);
             //signee cancel his shift
-            if($postData['signeeId'] == Auth::user()->id && $postData['status'] == "CREATED")
+            if($postData['signeeId'] == Auth::user()->id && isset($postData['signee_booking_status']) == "CANCEL")
             {
                 //echo "hi";exit();
                 $msg = 'Your shift in'.' '.$postData['hospital_name'].' '.'hospital of'.' '.$postData['ward_name'].' '.'ward at '.$date.' '.$time.' '.'has been canceled';
@@ -66,13 +63,10 @@ class Notification extends Model
             }
             elseif(!isset($postData['signee_booking_status']) && $postData['status'] == "CONFIRMED")
             {
-                //echo "456";exit;
                 $msg = 'Your shift in'.' '.$postData['hospital_name'].' '.'hospital of'.' '.$postData['ward_name'].' '.'ward at '.$date.' '.$time.' '.'has been confirmed';
             }
             elseif(isset($postData['signee_booking_status']) == "CANCEL")
             {
-               // echo "123";exit;
-                // dd('hi');
                 $msg = 'Your shift in'.' '.$postData['hospital_name'].' '.'hospital of'.' '.$postData['ward_name'].' '.'ward at '.$date.' '.$time.' '.'has been cancelled by admin';
             }
         }
@@ -88,18 +82,9 @@ class Notification extends Model
         }else{
             $notification->status=$postData['status'];
          }
-        //  1 ?? 0
-        // $notification->status =$postData['signee_booking_status']) ? $postData['signee_booking_status'] : $postData['status'];
         $notification->is_read = 0;
         $notification->is_sent = 0;
-               // print_r($notification);exit();
-
-        // print_r(isset($msg) ? "CREATED" : (isset($shiftConfirmedMsg) ? "CONFIRMED" : (isset($shiftCanceledMsg) ? "CANCELED" : (isset($complientMsg) ? $postData['status'] : "CANCELED"))));exit();
-
         $notification->save();
-      //  print_r('123');exit;
-
-        //$notification = '';
         return true;
     }
 }
