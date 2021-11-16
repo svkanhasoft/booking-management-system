eb7da44ad591e3f999a6e6158e1dd2b95f137e9c
maulikkanhasoft <maulik.kanhasoft@gmail.com>
2021-11-16 12:53:23 +0530

    change in speciality.php

--- app/Models/Speciality.php
+++ app/Models/Speciality.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Speciality extends Model
 {
-    //use SoftDeletes;
+    use SoftDeletes;
     /**
      * The database table used by the model.
      *
